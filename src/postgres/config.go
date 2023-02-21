a70d08b4245512031ff7234e360e7957b3067a1f
SalomanYu <yunoshev.dev@gmail.com>
2023-02-21 10:23:27 +0300

    переменные окружения

--- src/postgres/config.go
+++ src/postgres/config.go
@@ -1,17 +1,32 @@
 package postgres
 
 import (
-	"fmt"
 	"database/sql"
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
-const (
-	host = "localhost"
-	port = 5432
-	user = "postgres"
-	password = "admin"
-	dbname = "vacancies"
+func init() {
+	err := godotenv.Load(".env")
+	checkErr(err)
+	host = os.Getenv("POSTGRES_HOST")
+	port, _ = strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+	user = os.Getenv("POSTGRES_USER")
+	password = os.Getenv("POSTGRES_PASSWORD")
+	dbname = os.Getenv("POSTGRES_DATABASE")
+	fmt.Println(host)
+}
+
+var (
+	host string
+	port int
+	user string
+	password string
+	dbname  string
 
 	TableCity = "city"
 	TableProfessions = "position"
